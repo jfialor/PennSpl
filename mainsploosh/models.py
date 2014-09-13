aefe5537b9e32aa1bf7b6b894698ccbb44cf91fc
Sedem Fialor <sedemf@gmail.com>
2014-09-13 16:32:04 -0400

    Ed's Model Edit

--- mainsploosh/models.py
+++ mainsploosh/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Class(models.Model):
+    class_id = models.CharField(max_length=10)
+    section  = models.CharField(max_length=10)
+    def __str__(self):
+        return self.class_id + '-' + completeSection(str(self.section))
+
+class ClassCategory(models.Model):
+    category_name = models.CharField('Name of the category as appears on PennIntouch', max_length=30)
+    ok_classes = models.ManyToManyField(Class, verbose_name='List of classes that satisfy the class requirement')
+    def __str__(self):
+        return self.category_name
+
+class Degree(models.Model):
+    major = models.CharField('Major', max_length=50, default = 'Undeclared')
+    degreeType = models.CharField('Type of degree', max_length=50, default = '')
+    school = models.CharField('School', max_length=50, default = '')
+    class_categories = models.ManyToManyField(ClassCategory, verbose_name="List of class category requirements")
+    def __str__(self):
+        return self.degreeType + ' in ' + self.major
+
+def completeSection(s):
+    if len(s) == 1:
+        return '00' + s
+    elif len(s) == 2:
+        return '0' + s
+    else:
+        return s
